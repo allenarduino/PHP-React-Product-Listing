71e5d00ca879080a9048b3e859eb2453a49de5ce
ALLEN <aljay3334@gmail.com>
2022-09-10 13:56:03 +0000

    Added 3 classes inheriting from ProductModel class

--- Models/ProductModel.php
+++ Models/ProductModel.php
@@ -1,4 +1,5 @@
 <?php
+include ("../Controllers/ProductController.php");
 abstract class ProductModel{
 protected $SKU;
 protected $name;
@@ -15,7 +16,7 @@ $this->type=$type;
 
 //getters
 public function getSKU(){
-    return $this->$SKU;
+    return $this->SKU;
 }
 
 public function getName(){
@@ -26,7 +27,7 @@ public function getPrice(){
     return $this->price;
 }
 public function getType(){
-    return $this->getType;
+    return $this->type;
 }
 
 //abstract method
@@ -34,6 +35,76 @@ abstract public function addProduct($data);
 
 
 }
+
+class Dvd extends ProductModel{
+    public $data;
+    protected $size;
+
+    public function __construct($SKU,$name,$price,$type,$size){
+        $this->size=$size;
+        parent::__construct($SKU,$name,$price,$type);
+    }
+    public function setSize($size){
+        $this->size=$size;
+    }
+
+    public function getSize(){
+       return $this->size;
+    }
+
+    public function addProduct($dvd){
+        $product_controller=new ProductController();
+        $product_controller->addDVD($dvd);
+    }
+}
+
+
+class Furniture extends ProductModel
+{
+    protected $height;
+    protected $width;
+    protected $length;
+    public function __construct($SKU, $name, $price, $type,$height,$width,$length) {
+        $this->height = $height;
+        $this->width = $width;
+        $this->length = $length;
+        parent::__construct($SKU, $name, $price, $type);
+    }
+    public function getHeight(){
+        return $this->height;
+    }
+    public function getWidth()
+    {
+        return $this->width;
+    }
+    public function getLength()
+    {
+        return $this->length;
+    }
+    public function addProduct($furniture){
+        $product_controller=new ProductController();
+        $product_controller->addFurnture($furniture);
+    }
+
+}
+
+class Book extends ProductModel
+{
+    protected $weight;
+    public function __construct($SKU, $name, $price, $type,$weight) {
+        $this->weight = $weight;
+        parent::__construct($SKU, $name, $price, $type);
+    }
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+    public function addProduct($book){
+        $product_controller = new ProductController();
+        $product_controller->addBook($book);
+    }
+}
+
 
 
 
